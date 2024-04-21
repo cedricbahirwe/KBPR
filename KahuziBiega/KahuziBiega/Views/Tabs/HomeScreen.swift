9b92f7d56405a4ed8b40e9261201a6aab99f0cd3
Cedric Bahirwe <cedricaganzebahirwe@gmail.com>
2024-04-21 14:24:34 +0200

    Add Icons

--- KahuziBiega/KahuziBiega/Views/Tabs/HomeScreen.swift
+++ KahuziBiega/KahuziBiega/Views/Tabs/HomeScreen.swift
@@ -9,11 +9,94 @@ import SwiftUI
 
 struct HomeScreen: View {
     @Binding var navPath: [AppRoute]
+    let dateFormatter: DateFormatter = {
+           let formatter = DateFormatter()
+           formatter.dateFormat = "dd MMM yyyy, h:mm a"
+           return formatter
+       }()
+    
     var body: some View {
-        Text("Home Screen")
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Text("Illegal hunting activity near the Visitor Center Area")
+                        .fontWeight(.semibold)
+                    
+                    Text("On the morning patrol, encountered a group of visitors near the Eastern Trail Area who had an unexpected ..")
+                        .fontWeight(.light)
+                    
+                    HStack {
+                        Text("Location: \(Text("Visitor Center Area").bold())")
+                            .foregroundStyle(.accent)
+                    }
+                    
+                    HStack {
+                        
+                        Group {
+                            Label(dateFormatter.string(from: .now), systemImage: "calendar")
+                                .font(.callout)
+                                .fontWeight(.semibold)
+                                .padding(8)
+                                .background(.tint.quinary.opacity(0.5), in: .rect(cornerRadius: 8))
+                                .foregroundStyle(.accent)
+                            
+                            Spacer()
+                            
+                            Text("In Review")
+                                .font(.callout)
+                                .fontWeight(.semibold)
+                                .padding(8)
+                                .background(.tint.quinary.opacity(0.5), in: .capsule)
+                                .foregroundStyle(.accent)
+                        }
+                    }
+                }
+                .padding(.horizontal, 10)
+                
+                
+                Spacer()
+                
+            }
+        }
+        .safeAreaInset(edge: .top) {
+            topBarView
+                .background(.regularMaterial, ignoresSafeAreaEdges: .top)
+        }
     }
 }
 
 #Preview {
     HomeScreen(navPath: .constant([]))
+}
+
+private extension HomeScreen {
+    var topBarView: some View {
+        HStack {
+            HStack(spacing: 16) {
+                Button(action: {
+                    
+                }) {
+                    Image(.mapMarker)
+                }
+                Button(action: {}) {
+                    Image(.siren)
+                }
+            }
+            
+            Text("Kauzi Biega Park")
+                .bold()
+                .frame(maxWidth: .infinity)
+            
+            HStack(spacing: 16) {
+                Button(action: {}) {
+                    Image(.radioTower)
+                }
+                
+                Button(action: {}) {
+                    Image(.avatarImg)
+                }
+            }
+        }
+        .padding()
+    }
 }
